595b997c932c2fd0d512344d48314770c6a6ca0f
Femi <alayesanmifemi@gmail.com>
2022-05-18 22:38:29 +0100

    added tests for endpoints

--- test/short-url.e2e.ts
+++ test/short-url.e2e.ts
@@ -7,7 +7,7 @@ import { HTTP } from '../src/consts';
 
 const request = supertest(app);
 
-describe('Books', () => {
+describe('Short urls', () => {
   let usedShortcode = ""
   it('create url shortcode', async () => {
     const urlResult = await request
